498691c883b9fb99ef8d23be6a1c213618fe3fc2
BhuvaneswariDeviGhanta <S562904@nwmissouri.edu>
2023-11-01 11:58:32 -0500

    added dog images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dog', dogRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 
